294946e4ffffc4bd9f80e91f70c5bc24d34c90be
ebaskaya <egemen.baskaya@outlook.com>
2022-11-29 14:21:58 +0300

    Add basic math functions

---
+++ src/script.js
@@ -0,0 +1,22 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2){
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+
+
+console.log(divide(6, 4));
+console.log(multiply(3, 4.5));
+console.log(subtract(3, 1));
+console.log(add(5, 4));
